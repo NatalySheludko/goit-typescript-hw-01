23389cb66f2adc5ba4778b444383fc9f5f5638d5
nataliia <sheludkonataliyavaleriivna@gmail.com>
2024-06-20 23:31:39 +0300

    add fifth task

---
+++ src/basic/5.ts
@@ -0,0 +1,17 @@
+enum DayOfWeek {
+  Monday = "MONDAY",
+  Tuesday = "TUESDAY",
+  Wednesday = "WEDNESDAY",
+  Thursday = "THURSDAY",
+  Friday = "FRIDAY",
+  Saturday = "SATURDAY",
+  Sunday = "SUNDAY",
+}
+
+const isWeekend = (day: DayOfWeek): boolean => {
+  if (day === DayOfWeek.Saturday || day === DayOfWeek.Sunday) {
+    return true;
+  } else {
+    return false;
+  }
+};
